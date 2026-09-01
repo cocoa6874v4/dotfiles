7d2d186f75a8f8cc2c4b77fa4cca1534d4899b8f
cocoa6874 <admin@eternitysystem.net>
2026-09-01 22:27:51 +0900

    add yazi

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,3 +34,11 @@ function dots
     git -C ~/dotfiles push
 end
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	command rm -f -- "$tmp"
+end
