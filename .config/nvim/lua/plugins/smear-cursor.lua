ac751ab63f2a5b4aefcf42c52c32047cbf204406
cocoa6874 <admin@eternitysystem.net>
2026-09-05 15:22:49 +0900

    add smear-cursor.nvim

---
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "https://github.com/sphamba/smear-cursor.nvim",
+        config = function()
+            require("smear_cursor").setup({
+                cursor_color = "#ffffff",
+
+                never_draw_over_target = true,
+
+                smear_insert_mode = false,
+                min_vertical_distance_smear = 2,
+                min_horizontal_distance_smear = 2,
+
+                time_interval = 17,
+                stiffness = 0.9,
+                trailing_stiffness = 0.4,
+                damping = 0.99,
+            })
+        end,
+    },
+}
