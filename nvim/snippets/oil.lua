1197643b5e58dad4da9748593933413e814140f5
cocoa6874 <admin@eternitysystem.net>
2026-09-09 21:15:54 +0900

    change nvim snippets

--- nvim/snippets/oil.lua
+++ nvim/snippets/oil.lua
@@ -1,43 +1,16 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local f = ls.function_node
-
-local function create_file(path)
-    if vim.uv.fs_stat(path) then
-        return
-    end
-
-    vim.fn.writefile({}, path)
-end
+local t = ls.text_node
 
 return {
     s("cpp", {
-        f(function()
-            local oil = require("oil")
-            local root = oil.get_current_dir()
-
-            if not root then
-                vim.notify("Could not get Oil directory", vim.log.levels.ERROR)
-                return ""
-            end
-
-            local src = vim.fs.joinpath(root, "src")
-
-            vim.fn.mkdir(src, "p")
-
-            create_file(vim.fs.joinpath(root, "LICENSE"))
-            create_file(vim.fs.joinpath(src, "main.cpp"))
-            create_file(vim.fs.joinpath(root, "CMakeLists.txt"))
-            create_file(vim.fs.joinpath(root, "cmake-variants.yaml"))
-            create_file(vim.fs.joinpath(root, ".gitignore"))
-            create_file(vim.fs.joinpath(root, "README.md"))
-
-            vim.schedule(function()
-                require("oil").refresh()
-                vim.notify("C++ project created")
-            end)
-
-            return ""
-        end),
+        t({
+            "LICENSE",
+            "src/main.cpp",
+            "CMakeLists.txt",
+            "cmake-variants.yaml",
+            ".gitignore",
+            "README.md",
+        }),
     }),
 }
